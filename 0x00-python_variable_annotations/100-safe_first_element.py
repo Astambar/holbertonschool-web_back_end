22c7cc882a214a89f43658be02ae0101f94fe5d0
Astambar <leuchartjason@gmail.com>
2023-01-23 19:11:02 +0000

    suis-je en bonne voie ?

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """safe_first_element
 """
-from typing import Any, Union, Sequence, NoneType
+from typing import Any, Union, Sequence
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Return the first element of a sequence or None.
 
     Args:
         lst (Sequence[Any]): The input sequence.
 
     Returns:
-        Union[Any, NoneType]: The first element of the input sequence or None.
+        Union[Any, None]: The first element of the input sequence or None.
     """
     if lst:
         return lst[0]
