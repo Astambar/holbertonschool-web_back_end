d15345c9f1371ec0f33771373c3cb4685ff9644b
Astambar <leuchartjason@gmail.com>
2023-01-23 15:27:50 +0000

    suis-je en bonne voie ?

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,11 +8,12 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by multiplier.
 
     Args:
-        multiplier (float): float value to multiply by
+        multiplier (float): the float to multiply by
 
     Returns:
-        Callable[[float], float]: function that multiplies a float by multiplier
+        Callable[[float], float]: function that multiplies a float
     """
+
     def multiply_by(n: float) -> float:
         return n * multiplier
     return multiply_by
