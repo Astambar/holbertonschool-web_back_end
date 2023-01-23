bffa72862e6fb13aefd2e2da427235f982a15992
Astambar <leuchartjason@gmail.com>
2023-01-23 12:09:51 +0000

    suis-je en bonne voie ?

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""floor
+"""
+import math
+
+def floor(n: float) -> int:
+    """Return the floor of a float.
+
+    Args:
+        n (float): a float to get the floor of.
+
+    Returns:
+        int: the floor of the float.
+    """
+    return math.floor(n)
