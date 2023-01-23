fc18e82c57e8d1614978a5e8564e706726710f9b
Astambar <leuchartjason@gmail.com>
2023-01-23 12:01:26 +0000

    suis-je en bonne voie ?

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""_concat_
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return str1 + str2
