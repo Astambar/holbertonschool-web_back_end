bf503b251d94fbcfd9fd3f95da00cab9639411d6
Astambar <leuchartjason@gmail.com>
2023-01-23 18:39:08 +0000

    suis-je en bonne voie ?

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""make_multiplier
+"""
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """_summary_
+
+    Args:
+        lst (Iterable[Sequence]): _description_
+
+    Returns:
+        List[Tuple[Sequence, int]]: _description_
+    """    
+    return [(i, len(i)) for i in lst]
