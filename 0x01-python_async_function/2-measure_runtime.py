1f9a7a1710eb51fa15a1964f9e0d303358822e96
Astambar <leuchartjason@gmail.com>
2023-01-24 14:12:09 +0000

    project python basic async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,8 +11,3 @@ def measure_time(n: int, max_delay: int) -> float:
 	result = wait_n(n, max_delay)
 	total_time = time.time() - start_time
 	return total_time / n
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))
