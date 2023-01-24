5ed05a981da79912b807bb2193d90620fc6782b3
Astambar <leuchartjason@gmail.com>
2023-01-24 14:06:14 +0000

    project python basic async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,16 +5,17 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
-	"""_summary_
+    """_summary_
 
-	Args:
-		n (int): _description_
-		max_delay (int): _description_
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
 
-	Returns:
-		List[float]: _description_
-	"""
-	tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
-	delays = [await task for task in asyncio.as_completed(tasks)]
-	return sorted(delays)
+    Returns:
+        List[float]: _description_
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    delays = [await task for task in asyncio.as_completed(tasks)]
+    return sorted(delays)
