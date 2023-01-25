b332d66a58b01b6678368fb0fc2d1f16b1282766
Astambar <leuchartjason@gmail.com>
2023-01-25 21:20:52 +0000

    adding full project

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 0-async_generator.py """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """_summary_
+
+    Yields:
+        Generator[float, None, None]: _description_
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
