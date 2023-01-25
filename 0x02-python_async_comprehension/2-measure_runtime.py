a4ca1a41ace5fe81d26eb9b78eee3d3dfd4f9a41
Astambar <leuchartjason@gmail.com>
2023-01-25 21:37:26 +0000

    optimisation et sécurité

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,15 +3,15 @@
 import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
-
-
+perf_counter = time.perf_counter
+gather = asyncio.gather
 async def measure_runtime() -> float:
     """_summary_
 
     Returns:
         float: _description_
     """
-    start = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for i in range(4)))
-    end = time.perf_counter()
+    start = perf_counter()
+    await gather(*(async_comprehension() for i in range(4)))
+    end = perf_counter()
     return (end - start)
