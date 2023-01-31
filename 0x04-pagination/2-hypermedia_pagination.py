ccaf0cdc846d47aae4d29bbcb3f69e85c63ecc4b
Astambar <leuchartjason@gmail.com>
2023-01-31 21:56:59 +0000

    correction 1

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python3
+""" 2-hypermedia_pagination.py """
+import csv
 import math
-"""_summary_
+from typing import List
 
-Returns:
-    _type_: _description_
-"""
+
+def index_range(page, page_size) -> tuple:
+    """ index_range function """
+    return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
-        self.data = []
-        with open("baby-names.csv", "r") as f:
-            for line in f:
-                self.data.append(line.split(","))
+        self.__dataset = None
 
-    def get_page(self, page, page_size):
-        start = (page - 1) * page_size
-        end = start + page_size
-        return self.data[start:end]
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-    def get_hyper(self, page=1, page_size=10):
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.data) / page_size)
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ get_page function """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        start, end = index_range(page, page_size)
+        if start >= len(self.dataset()):
+            return []
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ get_hyper function """
         return {
-            "page_size": page_size,
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+            'page_size': len(self.get_page(page, page_size)),
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page * page_size <
+            len(self.dataset()) else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': math.ceil(len(self.dataset()) / page_size)
         }
