6eea0754fca20ecc6944a7e47b23a5d11d9a512f
Astambar <leuchartjason@gmail.com>
2023-02-02 13:21:15 +0000

    2-lifo_cache.py

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 2-lifo_cache.py """
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
@@ -11,16 +11,17 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ put function """
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded_key = list(self.cache_data.keys())[0]
-            self.cache_data.pop(discarded_key)
-            print("DISCARD: {}".format(discarded_key))
-        self.cache_data[key] = item
+        if key and item:
+            if key in list(self.cache_data.keys()):
+                del self.cache_data[key]
+            if (len(self.cache_data.keys()) == self.MAX_ITEMS):
+                k = list(self.cache_data.keys()).pop()
+                del self.cache_data[k]
+                print("DISCARD: {}".format(k))
+            self.cache_data[key] = item
 
     def get(self, key):
         """ get function """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
