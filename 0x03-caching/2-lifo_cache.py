45ece6cb6bb98ab513697bb7bf7e35cf794a5b5e
Astambar <leuchartjason@gmail.com>
2023-02-02 13:19:55 +0000

    2-lifo_cache.py correctif

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,42 +1,26 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+""" 2-lifo_cache.py """
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """ LIFOCache """
+
     def __init__(self):
         super().__init__()
-        self.cache_data = {}
-        self.keys = []
 
     def put(self, key, item):
-        """
-        Assign to the dictionary self.cache_data
-        the item value for the key key.
-        If key or item is None,
-        this method should not do anything.
-        If the number of items in self.cache_data
-        is higher that BaseCaching.MAX_ITEMS,
-        discard the last item put in cache (LIFO algorithm)
-        and print a message.
-        """
+        """ put function """
         if key is None or item is None:
             return
-
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded_key = self.keys.pop()
+            discarded_key = list(self.cache_data.keys())[0]
             self.cache_data.pop(discarded_key)
             print("DISCARD: {}".format(discarded_key))
-
         self.cache_data[key] = item
-        self.keys.append(key)
 
     def get(self, key):
-        """
-        Return the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist
-        in self.cache_data, return None.
-        """
+        """ get function """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
