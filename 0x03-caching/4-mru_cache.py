d0197c83e155e52585adba4d55d6b65196d79057
Astambar <leuchartjason@gmail.com>
2023-02-02 20:31:45 +0000

    3-lru_cache.py correctif

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -6,30 +6,30 @@ from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class MRUCache(BaseCaching):
+class MostRecentlyUsedCache(BaseCaching):
     """ MRUCache """
 
     def __init__(self):
         super().__init__()
-        self.mru_order = OrderedDict()
+        self.most_recently_used = OrderedDict()
 
     def put(self, key, item):
         """ put function """
         if not key or not item:
             return
         self.cache_data[key] = item
-        self.mru_order[key] = item
+        self.most_recently_used[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            item_discarded = next(iter(self.mru_order))
+            item_discarded = next(iter(self.most_recently_used))
             del self.cache_data[item_discarded]
             print("DISCARD:", item_discarded)
-        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
-            self.mru_order.popitem(last=False)
-        self.mru_order.move_to_end(key, False)
+        if len(self.most_recently_used) > BaseCaching.MAX_ITEMS:
+            self.most_recently_used.popitem(last=False)
+        self.most_recently_used.move_to_end(key, False)
 
     def get(self, key):
         """ get function """
         if key in self.cache_data:
-            self.mru_order.move_to_end(key, False)
+            self.most_recently_used.move_to_end(key, False)
             return self.cache_data[key]
         return None
