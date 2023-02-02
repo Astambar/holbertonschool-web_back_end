dd69982cf01e12f547d5a442f2cd78e4492f5685
Astambar <leuchartjason@gmail.com>
2023-02-02 13:37:54 +0000

    2-lifo_cache.py correctif

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 """ 2-lifo_cache.py """
-BaseCaching = __import__('base_caching').BaseCaching
+import base_caching
 
 
-class LIFOCache(BaseCaching):
+class LIFOCache(base_caching.BaseCaching):
     """ LIFOCache """
 
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """ put function """
-        if key and item:
-            if key in self.cache_data:
-                del self.cache_data[key]
-            if len(self.cache_data) == self.MAX_ITEMS:
-                oldest_key = next(iter(self.cache_data))
-                self.cache_data.pop(oldest_key)
-                print("DISCARD: {}".format(oldest_key))
-            self.cache_data[key] = item
+        """
+        Ajoute un élément au cache en utilisant le comportement LIFO
+        (dernier entré, premier sorti).
+        Si le cache est plein, le dernier élément
+        ajouté sera supprimé pour faire de la place pour le nouvel élément.
+        """
+        if not key or not item:
+            return
+
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        if len(self.cache_data) == self.MAX_ITEMS:
+            k = list(self.cache_data.keys()).pop()
+            del self.cache_data[k]
+            print("DISCARD: {}".format(k))
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """ get function """
+        """
+        Récupère un élément du cache à partir de sa clé.
+        Retourne None si l'élément n'est pas présent dans le cache.
+        """
         return self.cache_data.get(key)
