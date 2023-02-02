71252ede155ca099e5e5af26819261a141c6c587
Astambar <leuchartjason@gmail.com>
2023-02-02 20:21:30 +0000

    2-lifo_cache.py correctif

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
 """ 2-lifo_cache.py """
-import base_caching
+from base_caching import BaseCaching
 
-
-class LIFOCache(base_caching.BaseCaching):
+class LIFOCache(BaseCaching):
     """ LIFOCache """
 
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """
-        Ajoute un élément au cache en utilisant le comportement LIFO
-        (dernier entré, premier sorti).
-        Si le cache est plein, le dernier élément
-        ajouté sera supprimé pour faire de la place pour le nouvel élément.
-        """
+        """ put function """
         if not key or not item:
             return
 
         if key in self.cache_data:
             del self.cache_data[key]
-
-        if len(self.cache_data) == self.MAX_ITEMS:
-            k = list(self.cache_data.keys()).pop()
-            del self.cache_data[k]
-            print("DISCARD: {}".format(k))
-
+        
+        keys = list(self.cache_data.keys())
+        if len(keys) == self.MAX_ITEMS:
+            removed_key = keys.pop()
+            del self.cache_data[removed_key]
+            print("DISCARD: {}".format(removed_key))
+        
         self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Récupère un élément du cache à partir de sa clé.
-        Retourne None si l'élément n'est pas présent dans le cache.
-        """
-        return self.cache_data.get(key)
+        """ get function """
+        return self.cache_data.get(key, None)
--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
+"""LRUCache module"""
+from base_caching import BaseCaching
+
+
 class LRUCache(BaseCaching):
+    """LRUCache class"""
+
     def __init__(self):
+        """ init function """
         super().__init__()
-        self.cache_data = {}
+        self.cache_list = []
 
     def put(self, key, item):
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            lru = min(self.cache_data, key=lambda x: self.cache_data[x][1])
-            print("DISCARD:", lru)
-            del self.cache_data[lru]
+        """ put function """
+        if key in self.cache_list:
+            self.cache_list.remove(key)
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            LeastRecentlyUsedKey = self.cache_list.pop()
+            del self.cache_data[LeastRecentlyUsedKey]
+            print("DISCARD: {}".format(LeastRecentlyUsedKey))
+        if key and item:
+            self.cache_list.insert(0, key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """ get function """
+        if key and key in self.cache_data:
+            self.cache_list.remove(key)
+            self.cache_list.insert(0, key)
+            return self.cache_data[key]
+        return None
