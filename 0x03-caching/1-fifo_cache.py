5f001e6c0b4524f2e47bb8e37b620bc1e3fcde2d
Astambar <leuchartjason@gmail.com>
2023-02-02 13:00:05 +0000

    1-fifo_cache.py correctif

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,20 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module for FIFOCache
+"""
+
+from base_caching import BaseCaching
+
+
 class FIFOCache(BaseCaching):
+    """
+    Class that implements a FIFO cache
+    """
 
     def __init__(self):
+        """
+        Initialize the cache
+        """
         super().__init__()
+        self.cache_data = {}
+        self.keys = []
 
     def put(self, key, item):
+        """
+        Add an item to the cache
+        """
         if key is None or item is None:
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded_key = next(iter(self.cache_data))
+            discarded_key = self.keys.pop(0)
             self.cache_data.pop(discarded_key)
-            print("DISCARD:", discarded_key)
+            print("DISCARD: {}".format(discarded_key))
 
         self.cache_data[key] = item
+        self.keys.append(key)
 
     def get(self, key):
+        """
+        Retrieve an item from the cache
+        """
         if key is None or key not in self.cache_data:
             return None
 
