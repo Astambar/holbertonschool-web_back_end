6dfbc594e971272c8f5052d3af50f4fc79b2c4ec
Astambar <leuchartjason@gmail.com>
2023-02-03 17:31:21 +0000

    correctif level 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,10 +4,10 @@ import typing
 List = typing.List
 
 
-def filter_datum(fields: List[str], redaction_str: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ filter_datum function """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
-                              f'{field}={redaction_str}{separator}', message)
+                         f'{field}={redaction}{separator}', message)
     return message
