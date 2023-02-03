0c809974dd8e9df26c0088aaf981cfe53ac8ee8f
Astambar <leuchartjason@gmail.com>
2023-02-03 17:30:23 +0000

    correctif level 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,13 +2,12 @@
 import re
 import typing
 List = typing.List
-substitute = re.sub
 
 
 def filter_datum(fields: List[str], redaction_str: str, message: str,
                  separator: str) -> str:
     """ filter_datum function """
     for field in fields:
-        message = substitute(f'{field}=(.*?){separator}',
+        message = re.sub(f'{field}=(.*?){separator}',
                               f'{field}={redaction_str}{separator}', message)
     return message
