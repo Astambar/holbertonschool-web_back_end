c5621d087835e55ff6a0652c1153d158b92b60b6
Astambar <leuchartjason@gmail.com>
2023-02-03 17:27:31 +0000

    correctif level 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import re
+import typing
+List = typing.List
+substitute = re.sub
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates sensitive fields in log messages
-    """
+def filter_datum(fields: List[str], redaction_str: str, log_line: str,
+                 separator: str) -> str:
+    """ filter_datum function """
     for field in fields:
-        message = re.sub(r"{}=[^;]*".format(field),
-                         "{}={}".format(field, redaction), message)
-    return message
+        log_line = substitute(f'{field}=(.*?){separator}',
+                              f'{field}={redaction_str}{separator}', log_line)
+    return log_line
