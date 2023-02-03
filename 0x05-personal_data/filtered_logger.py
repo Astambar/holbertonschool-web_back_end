c14ce99eaa528cfaab49029c0dd1d98454b8f9d7
Astambar <leuchartjason@gmail.com>
2023-02-03 17:33:20 +0000

    correctif level 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
+""" filtered_logger.py """
 import re
-import typing
-List = typing.List
+import logging
+from typing import List
+from os import environ
+from mysql.connector import connection
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format function """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ get_logger function """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +45,17 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+def get_db() -> connection.MySQLConnection:
+    """ get_db function """
+    username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    name = environ.get('PERSONAL_DATA_DB_NAME')
+    return connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=host,
+        database=name
+    )
