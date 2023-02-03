e07072639084679379f5547ccd11805becf41964
Astambar <leuchartjason@gmail.com>
2023-02-03 17:54:23 +0000

    verif check modif ?

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 Returns:
     _type_: _description_
 """
+import logging
 import re
 import typing
 List = typing.List
@@ -16,3 +17,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        record.getMessage(), self.SEPARATOR)
+        record.msg = filtered_message
+        return super().format(record)
