46e576c5fbefdea381e888cf383f700ce4c358b8
Astambar <leuchartjason@gmail.com>
2023-02-03 17:29:07 +0000

    correctif level 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,10 +5,10 @@ List = typing.List
 substitute = re.sub
 
 
-def filter_datum(fields: List[str], redaction_str: str, log_line: str,
+def filter_datum(fields: List[str], redaction_str: str, message: str,
                  separator: str) -> str:
     """ filter_datum function """
     for field in fields:
-        log_line = substitute(f'{field}=(.*?){separator}',
-                              f'{field}={redaction_str}{separator}', log_line)
-    return log_line
+        message = substitute(f'{field}=(.*?){separator}',
+                              f'{field}={redaction_str}{separator}', message)
+    return message
