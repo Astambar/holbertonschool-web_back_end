dbef6f42bf4a19db6c7b2f013abbfb0a060748ce
Astambar <leuchartjason@gmail.com>
2023-02-03 19:10:02 +0000

    project crypted test third version simplify

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,11 +6,30 @@ Returns:
 """
 import bcrypt
 
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
 
-def is_valid(hashed_password, password):
+def hash_password(password: str) -> bytes:
+    """_summary_
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        bytes: _description_
+    """
+    password = password.encode('utf-8')
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """_summary_
+
+    Args:
+        hashed_password (bytes): _description_
+        password (str): _description_
+
+    Returns:
+        bool: _description_
+    """
     password = password.encode('utf-8')
     return bcrypt.checkpw(password, hashed_password)
