55d342e0ef358ff333ba37ca7955e439b9156e61
Astambar <leuchartjason@gmail.com>
2023-05-15 07:20:25 +0000

    add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import math
+
+""" concatenation de chaîne """
+
+
+def floor(n: float) -> int:
+    """_floor_
+
+    Args:
+        n (str): _description_
+
+    Returns:
+        int: _description_
+    """
+    return math.floor(n)
