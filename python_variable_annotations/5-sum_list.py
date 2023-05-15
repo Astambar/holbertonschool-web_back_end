af8bb490c30f1b2cd0cae84a432827956c9f99f0
Astambar <leuchartjason@gmail.com>
2023-05-15 07:52:45 +0000

    somme de list de float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" somme d'une liste de float """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """_sum_list_
+
+    Args:
+        input_list (List[float]): _description_
+
+    Returns:
+        float: _description_
+    """
+    return sum(input_list)
