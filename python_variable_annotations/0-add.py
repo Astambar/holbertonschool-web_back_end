d522c058a588578e3cb8962360d93c9208293c5b
Astambar <leuchartjason@gmail.com>
2023-05-15 07:12:07 +0000

    0-add.py 1-concat.py correctif doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,5 +4,13 @@
 
 
 def add(a: float, b: float) -> float:
-    """add"""
+    """_add_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,5 +4,13 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """concat"""
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
     return str1 + str2
