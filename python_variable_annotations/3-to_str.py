a28c0bff2715eab23258a4d4e6fe49d2838e82dd
Astambar <leuchartjason@gmail.com>
2023-05-15 07:35:13 +0000

    conversion float en str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" conversion float en str """
+
+import math
+
+
+def to_str(n: float) -> str:
+    """_to_str_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str(n)
