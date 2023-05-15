2c45969ce5c5d7b8a53b9b19d280d7c385fd0b78
Astambar <leuchartjason@gmail.com>
2023-05-15 07:07:23 +0000

    0-add.py 1-concat.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" addition de deux float """
+
+
+def add(a: float, b: float) -> float:
+    """add"""
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" concatenation de chaîne """
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat"""
+    return str1 + str2
