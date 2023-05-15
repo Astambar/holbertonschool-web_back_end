5ff035559353b874b8886fa2bbcf7311f1707616
Astambar <leuchartjason@gmail.com>
2023-05-15 09:31:43 +0000

    détermine Typing object Iterable et forme de sortie attendu Tuple Sequence , int

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -9,7 +9,7 @@ from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """_summary_
+    """_element_length_
 
     Args:
         lst (Iterable[Sequence]): _description_
