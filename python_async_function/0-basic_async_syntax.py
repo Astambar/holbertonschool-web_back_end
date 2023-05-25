adb3fd9ee581af5bf26f82792b6837124a6d5707
Astambar <leuchartjason@gmail.com>
2023-05-25 18:37:50 +0000

    init project

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Ce module contient la fonction wait_random qui attend
+de manière asynchrone un délai aléatoire et le renvoie.
+"""
+
+import asyncio
+import random
+from typing import Callable
+
+uniform: Callable[[float, float], float] = random.uniform
+sleep: Callable[[float], None] = asyncio.sleep
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Attend de manière asynchrone un délai aléatoire
+       entre 0 et max_delay (inclus) et le renvoie.
+
+    Args:
+        max_delay (int): Le délai maximum à attendre. Par défaut à 10.
+
+    Returns:
+        float: Le délai réel attendu.
+    """
+    delay: float = uniform(0, max_delay)
+    await sleep(delay)
+    return delay
