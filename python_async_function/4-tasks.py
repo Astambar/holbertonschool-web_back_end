5c1b0eea93bd94bbe72bebd4b905fa37968dbc9a
Astambar <leuchartjason@gmail.com>
2023-05-26 01:37:57 +0000

    async function full project

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,16 +9,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Exécute la fonction task_wait_random n fois avec
-      un délai maximum de max_delay.
+    Exécute la fonction task_wait_random n fois
+       avec un délai maximum de max_delay.
 
     Retourne une liste des temps d'attente
-      pour chaque exécution de task_wait_random.
+       pour chaque exécution de task_wait_random.
     """
-
-    # Créer une liste de tâches en utilisant une compréhension de liste
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    # Attendre l'achèvement de toutes les tâches en utilisant asyncio.gather
-    wait_times = await asyncio.gather(*tasks)
-    # Retourner la liste des temps d'attente
-    return wait_times
+    list = []
+    for _ in range(n):
+        # Ajouter le résultat de task_wait_random à la liste
+        list.append(task_wait_random(max_delay))
+    # Retourner la liste des temps d'attente dans l'ordre d'achèvement
+    return [await i for i in asyncio.as_completed(list)]
