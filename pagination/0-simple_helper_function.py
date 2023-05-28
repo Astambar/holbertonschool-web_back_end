a4ff7debc807723ac1ddf272a4acbbd26cb4d75c
Astambar <leuchartjason@gmail.com>
2023-05-28 20:02:52 +0000

    pagination test

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,11 @@
 
 
 def index_range(page, page_size) -> tuple:
-    """ index_range function """
-    return ((page - 1) * page_size, page * page_size)
+    """
+    Cette fonction calcule les indices de début et de fin pour une page de données.
+    page: le numéro de la page
+    page_size: le nombre d'éléments par page
+    """
+    debut = (page - 1) * page_size
+    fin = page * page_size
+    return (debut, fin)
