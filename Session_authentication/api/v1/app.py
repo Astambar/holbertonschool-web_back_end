0b61add71e664b17e11a5228e62720ed0c739e9a
Astambar <leuchartjason@gmail.com>
2023-06-04 19:06:09 +0000

    test documented

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -14,41 +14,37 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = os.getenv("AUTH_TYPE")
 
-if auth_type == "auth":
+AUTH_TYPE = os.getenv('AUTH_TYPE', 'none')
+
+
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == "basic_auth":
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth  # Corrected import statement
     auth = SessionAuth()
-elif auth_type == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif auth_type == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
 @app.before_request
-def before_request():
-    """ before_request handler """
-    if auth is not None:
-        require_auth = auth.require_auth(request.path,
-                                         ['/api/v1/status/',
-                                          '/api/v1/unauthorized/',
-                                          '/api/v1/forbidden/',
-                                          '/api/v1/auth_session/login/'])
-        if require_auth is True:
-            if (auth.authorization_header(request) is None and
-                    auth.session_cookie(request) is None):
-                abort(401)
-            request.current_user = auth.current_user(request)
-            if request.current_user is None:
-                abort(403)
+def before_request() -> None:
+    """ before_request method
+    """
+    if auth is None:
+        return
+    if auth.require_auth(request.path, ['/api/v1/status/',
+                                        '/api/v1/unauthorized/',
+                                        '/api/v1/forbidden/']):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+
+    # Assign the result of auth.current_user(request) to request.current_user
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
@@ -58,13 +54,6 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(403)
-def Forbidden(error) -> str:
-    """ Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
 @app.errorhandler(401)
 def Unauthorized(error) -> str:
     """ Unauthorized handler
@@ -72,7 +61,14 @@ def Unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def Forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    port = int(getenv("API_PORT", "5000"))
     app.run(host=host, port=port)
