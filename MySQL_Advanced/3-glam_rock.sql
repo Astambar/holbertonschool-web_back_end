cde438402d4a0b2179bad8ae6ffc5279c1ce416a
Astambar <leuchartjason@gmail.com>
2023-07-02 21:36:14 +0000

    Mysql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,7 @@
 -- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT band_name,
-       CASE
-           WHEN split IS NULL THEN YEAR(CURRENT_DATE()) - YEAR(formed)
-           ELSE YEAR(split) - YEAR(formed)
-       END AS lifespan
+SELECT origin AS country,
+       COUNT(*) AS fan_count
 FROM bands
 WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+GROUP BY origin
+ORDER BY fan_count DESC;
