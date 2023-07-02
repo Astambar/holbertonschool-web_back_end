553a8b3f5ce184cd4f8824ceb67412669ea8e603
Astambar <leuchartjason@gmail.com>
2023-07-02 21:19:56 +0000

    MySQL advanced task

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,8 @@
 -- Sélectionne le nom du groupe et calcule la durée de vie en années
 SELECT band_name, 
        (YEAR(split) - YEAR(formed)) AS lifespan
-FROM bands
+FROM metal_bands
 -- Filtre les groupes ayant le style principal "Glam rock"
-WHERE main_style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 -- Trie les groupes par ordre décroissant de durée de vie
 ORDER BY lifespan DESC;
