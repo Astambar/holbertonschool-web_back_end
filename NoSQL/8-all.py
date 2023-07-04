111e7ef32f044b1fe6222f39a0eff9c70f510615
Astambar <leuchartjason@gmail.com>
2023-07-04 13:18:25 +0000

    passe test local

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,6 +5,6 @@
 def list_all(mongo_collection):
     """ lists all documents in a collection
     """
-    if mongo_collection:
+    if mongo_collection is not None:
         return mongo_collection.find()
     return []
