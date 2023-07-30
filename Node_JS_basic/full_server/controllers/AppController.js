9ed3f0da2e45c4de8491ae594dc1e7c6afaea274
Astambar <leuchartjason@gmail.com>
2023-07-30 21:23:58 +0000

    what corrupt AppController Test

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,10 +1,8 @@
 // full_server/controllers/AppController.js
 
-class AppController {
-    static getHomepage(req, res) {
-      res.status(200).send('Hello Holberton School!');
+export default class AppController {
+    static getHomepage(request, response) {
+      response.status(200);
+      response.send('Hello Holberton School!');
     }
   }
-  
-  module.exports = AppController;
-  
