af89148d89bcaa665a10c32bc6888c7b6e76620f
Astambar <leuchartjason@gmail.com>
2023-07-30 21:55:48 +0000

    correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
-// Affiche le message de bienvenue
-function displayWelcomeMessage() {
-    console.log('Welcome to Holberton School, what is your name?');
-  }
+/* eslint-disable jest/require-hook */
 
+// Affiche le nom de l'utilisateur
+function displayUserName(name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+  
   // Gère l'entrée de l'utilisateur
   function processUserInput() {
     process.stdin.on('readable', () => {
@@ -12,26 +14,26 @@ function displayWelcomeMessage() {
       }
     });
   }
-
-  // Affiche le nom de l'utilisateur
-  function displayUserName(name) {
-    process.stdout.write(`Your name is: ${name}`);
+  
+  // Affiche le message de bienvenue
+  function displayWelcomeMessage() {
+    console.log('Welcome to Holberton School, what is your name?');
   }
-
+  
   // Affiche un message de fermeture et termine le programme
   function closeProgram() {
     process.stdout.write('This important software is now closing\n');
     process.exit();
   }
-
+  
   // Fonction principale pour demander le nom à l'utilisateur
   function askName() {
     // Étape 1 : Affiche le message de bienvenue
     displayWelcomeMessage();
-
+  
     // Étape 2 : Écoute les événements pour gérer l'entrée de l'utilisateur
     processUserInput();
-
+  
     // Étape 3 : Écoute l'événement 'end' pour gérer la fermeture de l'entrée standard
     process.stdin.on('end', closeProgram);
   }
