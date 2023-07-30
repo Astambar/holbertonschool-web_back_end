575eded91afd8bb186ee9d63a01f76e7bf511415
Astambar <leuchartjason@gmail.com>
2023-07-30 14:02:46 +0000

    eslint correctif

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,9 @@
 // Fichier : 2-get_students_by_loc.js
 
-// La fonction getStudentsByLocation prend en argument une liste d'étudiants (un tableau d'objets) et une ville (une chaîne de caractères).
-// Elle renvoie un nouveau tableau contenant uniquement les étudiants situés dans la ville spécifiée.
+// La fonction getStudentsByLocation prend en argument une liste d'étudiants (un tableau d'objets)
+// et une ville (une chaîne de caractères).
+// Elle renvoie un nouveau tableau contenant
+// uniquement les étudiants situés dans la ville spécifiée.
 function getStudentsByLocation(studentList, city) {
   // Utilise la fonction filter pour filtrer les étudiants par leur lieu (attribut location).
   const studentsInCity = studentList.filter((student) => student.location === city);
