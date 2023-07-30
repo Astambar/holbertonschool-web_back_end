ca2d69db7e0a31e7c8b97854e239e87c982b177f
Astambar <leuchartjason@gmail.com>
2023-07-30 14:33:08 +0000

    correctif format char abandonne table

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,24 @@
-export default function cleanSet(set, string) {
-    if (!string || !string.length) return '';
-    let value = '';
-    for (const el of set) {
-      if (el && el.startsWith(string)) {
-        value
-          += value.length === 0 ? el.replace(string, '') : el.replace(string, '-');
+// Fichier : 8-clean_set.js
+
+// La fonction cleanSet retourne une chaîne de caractères contenant toutes les valeurs
+// de l'ensemble qui commencent par une chaîne spécifique.
+function cleanSet(set, string) {
+    // Crée une chaîne de caractères pour stocker les valeurs filtrées
+    let filteredValues = '';
+  
+    // Parcourt chaque élément de l'ensemble
+    for (const value of set) {
+      // Vérifie si l'élément commence par la chaîne spécifiée
+      if (value.startsWith(string)) {
+        // Si oui, ajoute le reste de la chaîne (après string) à filteredValues
+        filteredValues += filteredValues.length === 0 ? value.slice(string.length) : '-' + value.slice(string.length);
       }
     }
-    return value;
+  
+    // Renvoie les valeurs filtrées sous forme d'une chaîne de caractères
+    return filteredValues;
   }
+  
+  // Exporte la fonction cleanSet pour pouvoir l'utiliser dans d'autres fichiers.
+  export default cleanSet;
   
