fa35a4154fd3f2c9c0cbb14575cd39318c8ea2cf
Astambar <leuchartjason@gmail.com>
2023-07-30 21:29:23 +0000

    corrupt source search

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,21 @@
 // full_server/routes/index.js
 
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
 const express = require('express');
-const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
 
 const router = express.Router();
 
 router.get('/', AppController.getHomepage);
-router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-module.exports = router;
+router.get('/students', (req, res) => {
+  StudentsController.getAllStudents(req, res, process.argv[2]);
+});
+router.get('/students/:major', (req, res) => {
+  StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+});
+
+export default router;
+
+
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,45 +1,23 @@
-// full_server/utils.js
-
 const fs = require('fs');
 
-function readDatabase(filePath) {
+export default function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(Error('Cannot load the database'));
       } else {
-        // Parse the data from the database file and return the object of arrays of first names per fields
-        const databaseObject = parseDataFromDatabase(data);
-        resolve(databaseObject);
+        const [headerLine, ...lines] = data.split('\n').filter((line) => line.length > 0);
+        const headers = headerLine.split(',');
+
+        const listObj = lines.map((line) => line.split(',').reduce((object, currentValue, index) => Object.assign(object, { [headers[index]]: currentValue }), {}));
+
+        const groupByField = listObj.reduce((res, currentValue) => {
+          res[currentValue.field] = res[currentValue.field] || [];
+          res[currentValue.field].push(currentValue.firstname);
+          return res;
+        }, {});
+        resolve(groupByField);
       }
     });
   });
 }
-
-function parseDataFromDatabase(data) {
-  const lines = data.trim().split('\n');
-  const headers = lines[0].split(',');
-  const databaseObject = {};
-
-  // Initialize the databaseObject with an array for each field
-  headers.forEach(header => {
-    databaseObject[header.trim()] = [];
-  });
-
-  // Skip the first line (headers) and start from index 1
-  for (let i = 1; i < lines.length; i++) {
-    const values = lines[i].split(',');
-
-    // Iterate through each value and push it to the corresponding field's array
-    values.forEach((value, index) => {
-      const header = headers[index].trim();
-      databaseObject[header].push(value.trim());
-    });
-  }
-
-  return databaseObject;
-}
-
-module.exports = {
-  readDatabase,
-};
