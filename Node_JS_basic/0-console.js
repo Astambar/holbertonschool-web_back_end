eba97eaaf373a827d542c2fedf110ffad4f56b1d
Astambar <leuchartjason@gmail.com>
2023-07-30 21:41:50 +0000

    patch eslint

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,11 +1,11 @@
 // 0-console.js
 
 // Déclaration de la fonction displayMessage
-function displayMessage(message) {
+function displayMessage(message)
+{
     // Affiche le message dans la sortie standard (STDOUT)
     console.log(message);
-  }
-  
+}
+
   // Export de la fonction pour qu'elle puisse être utilisée dans d'autres fichiers
   module.exports = displayMessage;
-  
