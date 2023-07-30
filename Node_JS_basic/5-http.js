ff7e3465ee7352076ce5e2b75ed68b63841e08f4
Astambar <leuchartjason@gmail.com>
2023-07-30 20:57:47 +0000

    checker

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,14 @@
+// Importation du module HTTP requis pour créer un serveur
 const http = require('http');
+
+// Importation de la fonction countStudents depuis le fichier '3-read_file_async.js'
 const countStudents = require('./3-read_file_async');
 
+// Définition de l'adresse IP et du port sur lesquels le serveur va écouter
 const hostname = '127.0.0.1';
 const port = 1245;
+
+// Récupération du nom de la base de données depuis les arguments de ligne de commande
 const DB = process.argv[2];
 
 // Fonction pour gérer la route racine '/'
@@ -15,12 +21,14 @@ function handleRootRoute(req, res) {
 // Fonction pour gérer la route '/students'
 async function handleStudentsRoute(req, res) {
   try {
+    // Appel de la fonction countStudents pour obtenir les données des étudiants depuis la base de données
     const studentsData = await countStudents(DB);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
-    res.end(studentsData.join('\n'));
+    res.end(studentsData.join('\n')); // Envoi de la liste des étudiants au format texte avec un saut de ligne entre chaque nom
   } catch (error) {
+    // En cas d'erreur lors de l'accès à la base de données, renvoyer une réponse d'erreur avec le code 500
     res.statusCode = 500;
     res.setHeader('Content-Type', 'text/plain');
     res.end(`Cannot load the database: ${error.message}`);
@@ -35,13 +43,14 @@ function handleNotFound(req, res) {
 }
 
 // Création du serveur HTTP en utilisant le module 'http'
-const app = http.createServer(async (req, res) => {
-  if (req.url === '/students') {
-    handleStudentsRoute(req, res);
-  } else if (req.url === '/') {
-    handleRootRoute(req, res);
+const app = http.createServer((req, res) => {
+  // Vérification de l'URL de la requête pour déterminer quelle fonction de gestion de route utiliser
+  if (req.url === '/') {
+    handleRootRoute(req, res); // Gérer la route racine
+  } else if (req.url === '/students') {
+    handleStudentsRoute(req, res); // Gérer la route '/students'
   } else {
-    handleNotFound(req, res);
+    handleNotFound(req, res); // Gérer les autres routes avec une réponse '404 Not Found'
   }
 });
 
