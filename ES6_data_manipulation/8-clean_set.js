ab21bb33150190cbad06fff6ca4571d98cd6636e
Astambar <leuchartjason@gmail.com>
2023-07-30 14:52:02 +0000

    fix eslint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,21 +3,21 @@
 // La fonction cleanSet retourne une chaîne de caractères contenant toutes les valeurs
 // de l'ensemble qui commencent par une chaîne spécifique.
 function cleanSet(set, string) {
-    if (!string || !string.length) return '';
-    // Crée une chaîne de caractères pour stocker les valeurs filtrées
-    let filteredValues = '';
+  if (!string || !string.length) return '';
+  // Crée une chaîne de caractères pour stocker les valeurs filtrées
+  let filteredValues = '';
 
-    // Parcourt chaque élément de l'ensemble
-    for (const value of set) {
-      // Vérifie si l'élément commence par la chaîne spécifiée
-      if (value && value.startsWith(string)) {
-        // Si oui, ajoute le reste de la chaîne (après string) à filteredValues
-        filteredValues += filteredValues.length === 0 ? value.replace(string, '') : value.replace(string, '-');
-      }
+  // Parcourt chaque élément de l'ensemble
+  for (const value of set) {
+    // Vérifie si l'élément commence par la chaîne spécifiée
+    if (value && value.startsWith(string)) {
+      // Si oui, ajoute le reste de la chaîne (après string) à filteredValues
+      filteredValues += filteredValues.length === 0 ? value.replace(string, '') : value.replace(string, '-');
     }
-
-    // Renvoie les valeurs filtrées sous forme d'une chaîne de caractères
-    return filteredValues;
   }
-  // Exporte la fonction cleanSet pour pouvoir l'utiliser dans d'autres fichiers.
-  export default cleanSet;
+
+  // Renvoie les valeurs filtrées sous forme d'une chaîne de caractères
+  return filteredValues;
+}
+// Exporte la fonction cleanSet pour pouvoir l'utiliser dans d'autres fichiers.
+export default cleanSet;
