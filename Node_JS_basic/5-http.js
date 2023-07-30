5748e2a9307eb166daab4a62ab17af975cdb9d71
Astambar <leuchartjason@gmail.com>
2023-07-30 19:54:50 +0000

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -13,24 +13,24 @@ if (process.argv.length !== 3) {
 const database = process.argv[2];
 
 // Création du serveur HTTP
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     // Répondre avec "Hello Holberton School!" pour l'URL /
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    // Lire le contenu du fichier 3-read_file_async.js de manière asynchrone
-    fs.readFile('./3-read_file_async.js', 'utf8', (err, data) => {
-      if (err) {
-        // En cas d'erreur lors de la lecture du fichier
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error\n');
-      } else {
-        // En cas de succès, afficher le message suivi du contenu du fichier 3-read_file_async.js
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${data}`);
-      }
-    });
+    try {
+      // Call the countStudents function from 3-read_file_async.js and pass the database variable
+      const studentsData = await countStudents(database);
+
+      // Send the studentsData as the response
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${studentsData}`);
+    } catch (error) {
+      // Handle the error if countStudents function throws an error
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error\n');
+    }
   } else {
     // Répondre avec une erreur 404 pour toutes les autres URL
     res.writeHead(404, { 'Content-Type': 'text/plain' });
